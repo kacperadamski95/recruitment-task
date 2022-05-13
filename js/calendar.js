d025b7a7821ff38d72d826e5a20c287e22ebdaab
Kacper Adamski <kacper.adamski95@gmail.com>
2022-05-13 21:47:58 +0200

    added calendar from node.js, started coding desktop

---
+++ js/calendar.js
@@ -0,0 +1,7 @@
+console.log('hehe');
+import DateRangePicker from '../node_modules/vanillajs-datepicker/js/DateRangePicker.js'
+
+const elem = document.getElementById('foo');
+const rangepicker = new DateRangePicker(elem, {
+  // ...options
+});
