1174485a44cb7e22d0c8915ec3cf97f746da445f
Kacper Adamski <kacper.adamski95@gmail.com>
2022-05-15 21:45:47 +0200

    finished with static-page, animations, last corrections next

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,21 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    const menuIconHamburger = document.querySelector(".menu-toggle")
-    const navigationMobile = document.querySelector(".menu-hidden")
+    const menuIconHamburger = document.querySelector(".menu-toggle");
+    const navigationMobile = document.querySelector(".menu-hidden");
 
 
     menuIconHamburger.addEventListener("click", (event) => {
         navigationMobile.classList.toggle("menu-active");
     })
+    
+    const navigation = document.querySelector(".navigation--desktop");
+
+    window.onscroll = function() {
+        const top = window.scrollY;
+        if (top>=10) {
+            navigation.classList.add("header--backround");
+        } else {
+            navigation.classList.remove("header--backround");
+        }
+    }
+    
 });
